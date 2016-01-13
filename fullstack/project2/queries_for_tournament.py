d1b589f08b81b5ba47943718cf314b9d78adfe9f
Will Orzo <stevejobsss3000@gmail.com>
2016-01-12 23:53:04 -0500

    added lines for getting company mode completions

--- fullstack/project2/queries_for_tournament.py
+++ fullstack/project2/queries_for_tournament.py
@@ -1,4 +1,5 @@
 import psycopg2
+import os
 
 
 def doDB():
@@ -11,4 +12,57 @@ def doDB():
         print row
     DB.close()
 
-doDB()
+# doDB()
+
+
+def wtf():
+    my_dir = os.getcwd()
+    print my_dir
+    DB = psycopg2.connect("dbname=tourney_practice")
+    c = DB.cursor()
+    query = "select * from players1;"
+    c.execute(query)
+    rows = c.fetchall()
+    for row in rows:
+        print row
+
+    DB.close()
+    
+
+# def show_all():
+#     DB = psycopg2.connect("dbname=tourney_practice")
+#     c = DB.cursor()
+#     query = "select * from players1;"
+#     c.execute(query)
+#     rows = c.fetchall()
+#     for row in rows:
+#         print row
+
+#     DB.close()
+import psycopg2
+
+psycopg2.extensions.cursor.fetchone
+psycopg2.extensions.cursor.fetchall
+
+DB = psycopg2.connect("dbname=tourney_practice")
+
+c = DB.cursor()
+
+c.execute("select * from players1;")
+
+row = c.fetchall()
+
+type(row)
+
+row
+
+print row
+
+c.execute("select count(*) from players1;")
+
+num = c.fetchall()
+
+type(num)
+
+DB.close()
+
