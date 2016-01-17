8a975a83a2f8f76fd18708f1166bd80ddd214853
Will Orzo <stevejobsss3000@gmail.com>
2016-01-16 19:55:22 -0500

    update table schema to match udacity project's

--- fullstack/project3/lesson1/puppiedb.py
+++ fullstack/project3/lesson1/puppiedb.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Date, Numeric
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,25 +8,27 @@ Base = declarative_base()
 
 class Shelter(Base):
 
-
     __tablename__ = 'shelter'
 
-    name = Column(String(80), nullable=False)
-    address = Column(String(80))
-    city = Column(String(80))
-    state = Column(String(80))
-    zipCode = Column(Integer(5))
-    website = Column(String(80))
     id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    address = Column(String(250))
+    city = Column(String(80))
+    state = Column(String(20))
+    zipCode = Column(Integer(10))
+    website = Column(String)
+
 
 
 class Puppy(Base):
 
-    
     __tablename__ = 'name'
 
-    name = Column(String(80), nullable=False)
-    dob = Column(String(9))
-    gender = Column(String(2))
-    weight = Column(Integer(3))
-    shelter_id = Column(Integer, ForeignKey=('Shelter.id'))
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    gender = Column(String(6)), nullable = False)
+    dateOfBirth = Column(String(Date)
+    picture = Column(String)
+    shelter_id = Column(Integer, ForeignKey('Shelter.id'))
+    shelter = relationship(Shelter)
+    weight = Column(Numeric(10))
